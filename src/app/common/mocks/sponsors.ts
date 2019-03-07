2b0ad87808db2dccf4141825dce12273d6421940
Natalia Venditto <natalia.venditto@netcentric.biz>
2019-03-07 07:23:11 +0100

    latest version speakers

--- src/app/common/mocks/sponsors.ts
+++ src/app/common/mocks/sponsors.ts
@@ -3,91 +3,115 @@ export const SPONSORS = [
   {
     name: 'Mango',
     logo: 'mango',
-    type: 'silver'
+    type: 'silver',
+    web: 'https://shop.mango.com'
   },
   {
     name: 'Marfeel',
     logo: 'marfeel',
-    type: 'bronze'
+    type: 'bronze',
+    web: 'https://www.marfeel.com'
   },
   {
     name: 'Netcentric',
     logo: 'netcentric',
-    type: 'gold'
+    type: 'gold',
+    web: 'https://www.netcentric.biz'
   },
   {
     name: 'New Relic',
     logo: 'newrelic',
-    type: 'silver'
+    type: 'silver',
+    web: 'https://newrelic.com'
   },
   {
     name: 'ThoughtWorks',
     logo: 'thoughtworks',
-    type: 'silver'
+    type: 'silver',
+    web: 'https://www.thoughtworks.com'
   },
   {
     name: 'Typeform',
     logo: 'typeform',
-    type: 'bronze'
+    type: 'bronze',
+    web: 'https://typeform.com'
   },
   {
     name: 'Vueling',
     logo: 'vueling',
-    type: 'silver'
+    type: 'silver',
+    web: 'https://www.vueling.com'
   },
   {
     name: 'Zopa',
     logo: 'zopa',
-    type: 'bronze'
+    type: 'bronze',
+    web: 'https://www.zopa.com'
   },
   {
     name: 'HolaLuz',
     logo: 'holaluz',
-    type: 'gold'
+    type: 'gold',
+    web: 'https://www.holaluz.com'
   },
   {
     name: 'Letgo',
     logo: 'letgo',
-    type: 'silver'
+    type: 'silver',
+    web: 'https://es.letgo.com/es'
   },
   {
     name: 'Wallapop',
     logo: 'wallapop',
-    type: 'bronze'
+    type: 'bronze',
+    web: 'https://es.wallapop.com'
   },
   {
     name: 'Ocado',
     logo: 'ocado',
-    type: 'bronze'
+    type: 'bronze',
+    web: 'https://ocadotechnology.com'
   },
   {
     name: 'BTS',
     logo: 'bts',
-    type: 'bronze'
+    type: 'bronze',
+    web: 'https://barcelonatechnologyschool.com'
   },
   {
     name: 'King',
     logo: 'king',
-    type: 'silver'
+    type: 'silver',
+    web: 'https://kind.com/es'
   },
   {
     name: 'Autentia',
     logo: 'autentia',
-    type: 'support'
+    type: 'support',
+    web: 'https://www.authentia.com'
   },
   {
     name: 'CodelyTV',
     logo: 'codely',
-    type: 'support'
+    type: 'support',
+    web: 'https://codely.tech'
   },
   {
     name: 'Frontend Developer Love',
     logo: 'frontendlove',
-    type: 'support'
+    type: 'support',
+    web: 'https://frontenddeveloperlove.com'
+  },
+  {
+    name: 'Xing',
+    logo: 'xing',
+    type: 'bronze',
+    web: 'https://xing.com'
   },
   {
     name: 'Agile Spain',
     logo: 'agilespain',
-    type: 'support'
+    type: 'support',
+    web: ''
   }
 ];
